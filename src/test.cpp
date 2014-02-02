5aa393b422a65a4251b86a613574d7598c0ba86a
davidsminor <davidm@image-engine.com>
2014-02-02 12:06:52 -0800

    created mpmsnow library
    ...which my little sim test and the unit tests (which  don't really
    exist yet) depend on...

---
+++ src/test.cpp
@@ -0,0 +1,21 @@
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+#include "MpmSnow/Grid.h"
+#include "MpmSnow/SnowConstituativeModel.h"
+#include "MpmSnow/CollisionPlane.h"
+#include "MpmSnow/ConjugateGradients.h"
+#include "MpmSnow/ConjugateResiduals.h"
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
